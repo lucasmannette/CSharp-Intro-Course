c2e301b4262851c6e0cab92a7ef399e64968a812
lucasmannette <30373227+lucasmannette@users.noreply.github.com>
2018-01-03 21:40:16 -0600

    1.3.2018 progress

---
+++ CS-ASP_028/CS-ASP_028/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_028
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cupsRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateCups(1.0,"cups");
+        }
+
+        protected void fromPintsRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateCups(2.0,"Pints");
+        }
+
+        protected void fromQuartsRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateCups(4.0,"Quarts");
+        }
+
+        protected void fromGallonsRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateCups(16.0,"Gallons");
+        }
+
+        protected void quantityTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //calculateCups();
+        }
+
+        //New Method to remove dup code
+        private void calculateCups(double measureToCupRatio, string measureName)
+        {
+            if (quantityTextBox.Text.Trim().Length == 0)
+                return;
+
+            double quantity = 0.0;
+            if (!Double.TryParse(quantityTextBox.Text, out quantity))
+                return;
+
+            double cups = quantity * measureToCupRatio;
+
+            resultLabel.Text = String.Format("{0:N2} {1} is equal to {2:N2} cups.", quantity, measureName, cups) ;
+
+
+        }
+    }
+}
---
+++ ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeForXmenBattleCount
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Wolverine fewest battles
+            // Pheonix most battles
+
+            string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus" };
+            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+
+            string result = "";
+
+
+            // Your Code Here!
+            for (int i = 0; i < names.Length; i++)
+            {
+                if(numbers.Min() == numbers[i])
+                    result += String.Format("{0} won the fewest battles. With {1} win(s)<br>", names[i], numbers[i]);
+
+                if (numbers.Max() == numbers[i])
+                    result += String.Format("{0} won the most battles. With {1} win(s)<br>", names[i], numbers[i]);
+            }
+
+
+            resultLabel.Text = result;
+        }
+    }
+}
