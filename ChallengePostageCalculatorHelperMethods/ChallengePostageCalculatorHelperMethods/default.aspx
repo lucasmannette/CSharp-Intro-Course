438a4eaf0c74313f81856cfafb937fa7ed3aae9a
lucasmannette <30373227+lucasmannette@users.noreply.github.com>
2018-01-06 11:30:06 -0600

    1.6.2018 progress

---
+++ ChallengePostageCalculatorHelperMethods/ChallengePostageCalculatorHelperMethods/default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ChallengePostageCalculatorHelperMethods._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Postal Calculator<br />
+        <br />
+        Width:&nbsp;
+        <asp:TextBox ID="widthTextBox" runat="server"></asp:TextBox>
+        <br />
+        Height:&nbsp;
+        <asp:TextBox ID="heightTextBox" runat="server"></asp:TextBox>
+        <br />
+        Length:&nbsp;
+        <asp:TextBox ID="lengthTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:RadioButton ID="groundRadioButton" runat="server" GroupName="shippingMethods" Text="Ground" OnCheckedChanged="groundRadioButton_CheckedChanged" AutoPostBack="True" />
+        <br />
+        <asp:RadioButton ID="airRadioButton" runat="server" GroupName="shippingMethods" Text="Air" OnCheckedChanged="airRadioButton_CheckedChanged" AutoPostBack="True" />
+        <br />
+        <asp:RadioButton ID="nextDayRadioButton" runat="server" GroupName="shippingMethods" Text="Next Day" OnCheckedChanged="nextDayRadioButton_CheckedChanged" AutoPostBack="True" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePostageCalculatorHelperMethods/ChallengePostageCalculatorHelperMethods/default.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostageCalculatorHelperMethods
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+         
+        protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkWidthTextBox() && checkHeightTextBox())
+            {
+                if (checkLengthTextBox())
+                {
+                    calculatePostage(0.15, double.Parse(widthTextBox.Text), double.Parse(heightTextBox.Text), double.Parse(lengthTextBox.Text));
+                }
+                else
+                {
+                    calculatePostage(0.15, double.Parse(widthTextBox.Text), double.Parse(heightTextBox.Text));
+                }
+                
+            }
+        }
+
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkWidthTextBox() && checkHeightTextBox())
+            {
+                if (checkLengthTextBox())
+                {
+                    calculatePostage(0.25, double.Parse(widthTextBox.Text), double.Parse(heightTextBox.Text), double.Parse(lengthTextBox.Text));
+                }
+                else
+                {
+                    calculatePostage(0.25, double.Parse(widthTextBox.Text), double.Parse(heightTextBox.Text));
+                }
+
+            }
+        }
+
+        protected void nextDayRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkWidthTextBox() && checkHeightTextBox())
+            {
+                if (checkLengthTextBox())
+                {
+                    calculatePostage(0.45, double.Parse(widthTextBox.Text), double.Parse(heightTextBox.Text), double.Parse(lengthTextBox.Text));
+                }
+                else
+                {
+                    calculatePostage(0.45, double.Parse(widthTextBox.Text), double.Parse(heightTextBox.Text));
+                }
+
+            }
+        }
+
+        protected bool checkWidthTextBox()
+        {
+            if (widthTextBox.Text.Length > 0)
+                return true;
+            else
+                return false;
+        }
+
+        protected bool checkHeightTextBox()
+        {
+            if (heightTextBox.Text.Length > 0)
+                return true;
+            else
+                return false;
+        }
+
+        protected bool checkLengthTextBox()
+        {
+            if (lengthTextBox.Text.Length > 0)
+                return true;
+            else
+                return false;
+        }
+
+        //return postage value
+        protected void calculatePostage(double multiplier, double width, double height, double length = 1)
+        {
+            double totalPostage = (width * height * length) * multiplier;
+            resultLabel.Text = String.Format("Total Postage: {0:C}", totalPostage);
+        }
+
+    }
+}
