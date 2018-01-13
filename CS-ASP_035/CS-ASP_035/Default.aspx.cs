54d43d6ccc979961d7d1ac4c12b2217ca4eb77b8
lucasmannette <30373227+lucasmannette@users.noreply.github.com>
2018-01-13 08:20:50 -0600

    1.13.18 progress

---
+++ CS-ASP_035/CS-ASP_035/Default.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_035
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            // use \ to escape double quotes
+            //resultLabel.Text = "<p style=\"color:#ee3b32;\">Hi<p>";
+
+            string value = valueTextBox.Text;
+
+            //Access any specific character
+            //resultLabel.Text = value[2].ToString();
+
+            //StartWith(), EndWith(), Contains()
+            //if (value.StartsWith("A"))
+            //    resultLabel.Text = "The value starts with A";
+
+            //if (value.EndsWith("."))
+            //    resultLabel.Text = "The value end in a .";
+
+            //if (value.Contains("wise"))
+            //    resultLabel.Text = "The value contains wise!";
+
+            //IndexOf()
+            int index = value.IndexOf("wise");
+            //resultLabel.Text = "'wise' begins at index " + index.ToString();
+
+            //Insert,Remove
+            //resultLabel.Text = value.Insert(index, "jolly ");
+            //resultLabel.Text = value.Remove(index, value.Length - index);
+
+            //Substring
+            //resultLabel.Text = value.Substring(index, value.Length - index - 1);
+
+            //Trim, TrimStart, TrimEnd
+            //resultLabel.Text = String.Format("Length Before: {0}<br> Length After: {1}", value.Length, value.Trim().Length);
+
+            //PadLeft, PadRight
+            //char needs single quotes
+            //resultLabel.Text = value.PadLeft(10, '*');
+
+            //ToUpper, ToLower
+            //if ("Bob" == "bob")
+            //    resultLabel.Text = "Same";
+            //else
+            //    resultLabel.Text = "Different";
+
+            //if (valueTextBox.Text.Trim().ToUpper() == "LUKE")
+            //    resultLabel.Text = "Same";
+            //else
+            //    resultLabel.Text = "Different";
+
+            //Replace()
+            //string template = "[NAME] said it was ok. Maybe you should take it up with [NAME]";
+            //resultLabel.Text = template.Replace("[NAME]", valueTextBox.Text);
+
+            //Split
+            //string result = "";
+            StringBuilder sb = new StringBuilder();
+            string[] values = valueTextBox.Text.Split(',');
+            for (int i = 0; i < values.Length; i++)
+            {
+                //result += values[i] + " " + values[i].Length + "<br>";
+                sb.Append(values[i]);
+                sb.Append(" ");
+                sb.Append(values[i].Length);
+                sb.Append("<br>");
+            }
+            //resultLabel.Text = result;
+            resultLabel.Text = sb.ToString();
+        }
+    }
+}
---
+++ ChallengePhunWithStrings/ChallengePhunWithStrings/Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePhunWithStrings
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // 1.  Reverse your name
+            string name = "Lucas Mannette";
+            // In my case, the result would be:
+            // ettennaM sacuL
+            string reverseName = "";
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reverseName += name[i];
+            }
+
+            resultLabel.Text = reverseName;
+
+            // 2.  Reverse this sequence:
+            string names = "Luke,Leia,Han,Chewbacca";
+            // When you're finished it should look like this:
+            // Chewbacca,Han,Leia,Luke
+            string reverseNames = "";
+            string[] reverseArray = names.Split(',');
+            for (int i = reverseArray.Length - 1; i >= 0; i--)
+            {
+                reverseNames += reverseArray[i];
+                
+            }
+            reverseNames = reverseNames.Remove(reverseNames.Length - 1, 1);
+            resultLabel.Text = reverseNames;
+
+            // 3. Use the sequence to create ascii art:
+            // *****luke*****
+            // *****leia*****
+            // *****han******
+            // **Chewbacca***
+            string namesArt = "";
+            for (int i = 0; i < reverseArray.Length; i++)
+            {
+                namesArt += reverseArray[i].PadLeft(reverseArray[i].Length + (14 - reverseArray[i].Length) / 2, '*').PadRight(14, '*') + "<br>";
+            }
+
+            resultLabel.Text = namesArt;
+
+
+            // 4. Solve this puzzle:
+
+            string puzzle = "NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.";
+
+            // Once you fix it with string helper methods, it should read:
+            // Now is the time for all good men to come to the aid of their country.
+
+            //Instructor Solution
+            //string removeMe = "remove-me";
+            //int index = puzzle.IndexOf(removeMe); 
+            //puzzle = puzzle.Remove(index, removeMe.Length);
+            //puzzle = puzzle.ToLower();
+            //puzzle = puzzle.Replace('z', 't');
+            //puzzle = puzzle.Remove(0, 1);
+            //puzzle = puzzle.Insert(0, "N");
+
+            //My Solution
+            puzzle = puzzle.Substring(0,1)  + puzzle.Substring(1).Replace("Z","T").Replace("remove-me","").ToLower();
+
+            resultLabel.Text = puzzle;
+
+        }
+    }
+}
